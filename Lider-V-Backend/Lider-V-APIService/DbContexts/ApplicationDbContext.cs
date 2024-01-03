a82d16e98b89eeea58036ecbf1543248fe536b1a
adjutantee <izagakhmaevra@gmail.com>
2024-01-03 15:27:59 +0300

    init changes

---
+++ Lider-V-Backend/Lider-V-APIService/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Lider_V_APIService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lider_V_APIService.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Lider-V-Backend/Lider-V-DatabaseService/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lider_V_DatabaseService.DbContexts
+{
+    internal class ApplicationDbContext
+    {
+    }
+}
