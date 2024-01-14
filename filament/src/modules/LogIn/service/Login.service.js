4e6e1075672447644d4a1dd6cfdca6788e7ca569
daniyal002 <daniyalou@gmail.com>
2024-01-14 16:14:41 +0300

    update login.service

--- filament/src/modules/LogIn/service/Login.service.js
+++ filament/src/modules/LogIn/service/Login.service.js
@@ -3,6 +3,5 @@ import { baseApi } from "../../../helper/baseApi";
 export const LoginAPI = async (body) => {
   return baseApi.post("AccountAPI/Login", body).then((response) => {
     response.data;
-    console.log(response.data);
   });
 };
