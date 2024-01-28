caac5c72a9123e098a4ff980fb34b667b6748c05
daniyal002 <daniyalou@gmail.com>
2024-01-28 13:23:37 +0300

    delete backend

--- Lider-V-Backend/Lider-V-APIService/Models/Dto/CartHeaderDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lider_V_APIService.Models.Dto
-{
-    public class CartHeaderDto
-    {
-        public int CartHeaderId { get; set; }
-        public string UserId { get; set; }
-    }
-}
